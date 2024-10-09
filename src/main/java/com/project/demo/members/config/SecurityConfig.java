0ca541c9151aeb21c9f2f727f25e2569b00285db
StandingAsh <tjswopark0123@gmail.com>
2024-10-09 16:30:53 +0900

    login fixed

--- src/main/java/com/project/demo/members/config/SecurityConfig.java
+++ src/main/java/com/project/demo/members/config/SecurityConfig.java
@@ -16,14 +16,15 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        // CRSF 인증 해제
+        // CSRF 인증 해제
         http.csrf(AbstractHttpConfigurer::disable);
         http.sessionManagement((session) ->
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         // URL 별 접근 권한 부여
         http.authorizeHttpRequests((requests) ->
-                requests.requestMatchers("/", "/members/**").permitAll()
+                requests.requestMatchers("/", "/members/**", "/mypage")
+                        .permitAll()
                         .anyRequest().authenticated());
 
         // 로그인 설정
@@ -31,6 +32,7 @@ public class SecurityConfig {
                 .loginProcessingUrl("/members/loginProc")
                 .defaultSuccessUrl("/mypage")
                 .usernameParameter("userId")
+                .passwordParameter("password")
                 .permitAll()
         );
 
--- src/main/java/com/project/demo/members/service/MemberService.java
+++ src/main/java/com/project/demo/members/service/MemberService.java
@@ -3,21 +3,25 @@ package com.project.demo.members.service;
 import com.project.demo.members.dto.MemberDTO;
 import com.project.demo.members.entity.Member;
 import com.project.demo.members.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class MemberService {
 
     @Autowired
     private MemberRepository memberRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
     public void join(MemberDTO memberDto) {
 
         Member member = Member.builder()
                 .name(memberDto.getName())
                 .userId(memberDto.getUserId())
-                .password(memberDto.getPassword())
+                .password(passwordEncoder.encode(memberDto.getPassword()))
                 .email(memberDto.getEmail())
                 .build();
 
