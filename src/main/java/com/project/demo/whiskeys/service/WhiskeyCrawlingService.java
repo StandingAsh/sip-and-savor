a9e38f1f301d529ad94f8078403a3926b78f50d9
StandingAsh <tjswopark0123@gmail.com>
2024-11-11 16:10:22 +0900

    페이지 이동 코드 추가

--- src/main/java/com/project/demo/whiskeys/service/WhiskeyCrawlingService.java
+++ src/main/java/com/project/demo/whiskeys/service/WhiskeyCrawlingService.java
@@ -36,47 +36,60 @@ public class WhiskeyCrawlingService {
             WebElement webElement = driver.findElement(By.cssSelector("#content > div > section > div > section.srchResultContentArea > div > div > div.srchResultSortArea > div > div:nth-child(3) > button"));
             webElement.click();
 
-            WebElement whiskeyContainer = driver.findElement(By.cssSelector("div.s-goods-layout.s-goods-layout__list"));
+            // 더 넘길 페이지 없을 때까지 반복
+            while (true) {
+                WebElement whiskeyContainer = driver.findElement(By.cssSelector("div.s-goods-layout.s-goods-layout__list"));
 
-            for (WebElement whiskey : whiskeyContainer.findElements(By.cssSelector("div.s-goods-grid.s-goods-grid--col-4 > ul > li"))) {
-
-                // 보틀 정보 엘레멘트 저장
-                try {
-                    WebElement spec = whiskey.findElement(By.className("s-goods-spec__list"));
-                    List<WebElement> specValues = spec.findElements(By.tagName("span"));
-                    List<WebElement> specKeys = spec.findElements(By.tagName("em"));
-
-                    Map<String, String> specMap = new HashMap<>();
-                    for (int i = 0; i < specValues.size(); i++)
-                        specMap.put(specKeys.get(i).getText(), specValues.get(i).getText());
+                // 페이지에 항목이 없을 때까지 반복
+                for (WebElement whiskey : whiskeyContainer.findElements(By.cssSelector("div.s-goods-grid.s-goods-grid--col-4 > ul > li"))) {
 
                     // 위스키 이름, 사진 url, 정보 문자열로 저장
                     String name = whiskey.findElement(By.cssSelector("div > div.s-goods__info > a > div.s-goods-title")).getText();
                     String imgUrl = whiskey.findElement(By.cssSelector("div.s-goods__thumbnail > a > div.s-goods-image > img")).getAttribute("src");
 
-                    // 스펙 정보 저장
+                    // 스펙 정보 초기화
                     String category = "Unknown";
                     String abv = "Unknown";
                     String country = "Unknown";
                     String year = "NAS"; // 디폴트값
                     String bottleSize = "Unknown";
 
-                    if (specMap.containsKey("종류"))
-                        category = specMap.get("종류");
-                    if (specMap.containsKey("도수"))
-                        abv = specMap.get("도수");
-                    if (specMap.containsKey("나라"))
-                        country = specMap.get("나라");
-                    if (specMap.containsKey("년 수"))
-                        year = specMap.get("년 수");
-                    if (specMap.containsKey("용량"))
-                        bottleSize = specMap.get("용량");
+                    // 위스키 정보 엘리먼트 크롤링
+                    try {
+                        WebElement spec = whiskey.findElement(By.className("s-goods-spec__list"));
+                        List<WebElement> specValues = spec.findElements(By.tagName("span"));
+                        List<WebElement> specKeys = spec.findElements(By.tagName("em"));
 
-                    whiskeyRepository.save(new WhiskeyList(name, imgUrl, category, abv, country, year, bottleSize));
-                } catch (NoSuchElementException ignored) {
+                        Map<String, String> specMap = new HashMap<>();
+                        for (int i = 0; i < specValues.size(); i++)
+                            specMap.put(specKeys.get(i).getText(), specValues.get(i).getText());
+
+                        // 크롤링 정보 매핑
+                        if (specMap.containsKey("종류"))
+                            category = specMap.get("종류");
+                        if (specMap.containsKey("도수"))
+                            abv = specMap.get("도수");
+                        if (specMap.containsKey("나라"))
+                            country = specMap.get("나라");
+                        if (specMap.containsKey("년 수"))
+                            year = specMap.get("년 수");
+                        if (specMap.containsKey("용량"))
+                            bottleSize = specMap.get("용량");
+
+                        whiskeyRepository.save(new WhiskeyList(name, imgUrl, category, abv, country, year, bottleSize));
+                    } catch (NoSuchElementException ignored) {
+                        whiskeyRepository.save(new WhiskeyList(name, imgUrl, category, abv, country, year, bottleSize));
+                    }
+                }
+
+                // 페이지바 업데이트, 더 넘길 페이지 없을 경우 반복문 탈출
+                try {
+                    WebElement nextPage = driver.findElement(By.cssSelector("#content > div > section > div > section.srchResultContentArea > div > div > div.s-goods-layout.s-goods-layout__list > div.srchPagination > a.srchPaginationNext"));
+                    nextPage.click();
+                } catch (NoSuchElementException e) {
+                    break;
                 }
             }
-
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
