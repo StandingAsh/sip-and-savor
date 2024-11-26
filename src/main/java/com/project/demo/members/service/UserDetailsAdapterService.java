79e78838acc4fc42627ded46f70a182330c85c41
StandingAsh <tjswopark0123@gmail.com>
2024-11-26 15:50:26 +0900

    Class name modified

--- src/main/java/com/project/demo/members/service/UserDetailsAdapterService.java
+++ src/main/java/com/project/demo/members/service/UserDetailsAdapterService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class MemberDetailsService implements UserDetailsService {
+public class UserDetailsAdapterService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
 
